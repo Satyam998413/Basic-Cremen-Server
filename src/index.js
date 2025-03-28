e8e7ace3b3d40286b3bcf73d2b985dcdda2ddce0
DEVELOPER USER7 <udeveloper7@resonent.local>
2025-03-28 12:23:22 +0530

    add vercel

---
+++ src/index.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const cors = require('cors');
+const morgan = require('morgan');
+require('dotenv').config();
+const { connectDB } = require('./config/database');
+const { httpLogger } = require('./lib/winstonLogger');
+
+const app = express();
+
+// Middleware
+// app.use(cors({
+//   origin: 'https://movies-app-review.vercel.app',
+//   methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//   allowedHeaders: ['Content-Type', 'Authorization'],
+//   credentials: true,
+// }));
+
+app.use(cors())
+app.use(express.json());
+app.use(morgan('dev'));
+
+// Custom Middleware to log request details and execution time
+app.use(httpLogger);
+
+
+// Routes
+app.use('/api/auth', require('./routes/auth.routes'));
+app.use('/api/movies', require('./routes/movie.routes'));
+app.use('/api/reviews', require('./routes/review.routes'));
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: 'Something went wrong!' });
+});
+
+const PORT = process.env.PORT || 5000;
+
+// Connect to database and start server
+const startServer = async () => {
+  try {
+    await connectDB();
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error('Failed to start server:', error);
+    process.exit(1);
+  }
+};
+
+startServer(); 
