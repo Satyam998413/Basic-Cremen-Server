6a6d5226be2ab774eab738155ee14de8f4e5a0ea
DEVELOPER USER7 <udeveloper7@resonent.local>
2025-03-28 12:20:40 +0530

    add vercel

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 // Import packages
 const express = require("express");
-const home = require("./routes/home");
 
 // Middlewares
 const app = express();
 app.use(express.json());
 
 // Routes
-app.use("/home", home);
+app.get("/", async (req, res, next) => {
+    return res.status(200).json({
+      title: "Express Testing",
+      message: "The app is working properly!",
+    });
+  });
 
 // connection
 const port = process.env.PORT || 4000;
